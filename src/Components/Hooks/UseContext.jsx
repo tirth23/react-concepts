df5c429d6bfe8631415a45948aca593ce45f176b
Tirth Patel <tirthpatel23t@gmail.com>
2024-10-13 22:33:47 +0530

    useContext update

--- src/Components/Hooks/UseContext.jsx
+++ src/Components/Hooks/UseContext.jsx
@@ -1,5 +1,4 @@
 import React, { useState, createContext, useContext } from "react";
-import ReactDOM from "react-dom/client";
 
 /* when context value is changes, it will re-render the component that is using the context starting from parent */
 
@@ -8,8 +7,10 @@ const UserContext = createContext();
 export const UseContext = () => {
 	const [user, setUser] = useState("Jesse Hall");
 	console.log("component 1 rendered");
+  let currentValue = 100;
+  console.log(currentValue, 'in main component');
 	return (
-		<UserContext.Provider value={{ user, setUser }}>
+		<UserContext.Provider value={{ user, setUser, currentValue }}>
 			<h1>{`Hello ${user}!`}</h1>
 			<Component2 />
 		</UserContext.Provider>
@@ -31,7 +32,7 @@ function Component3() {
 	console.log("component 3 rendered");
 	return (
 		<>
-			<h1 onClick={() => setCheck(check + 1)}>Component 3</h1>
+			<h1 onClick={() => setCheck(check + 1)}>Click Component 3 to re-render 3, 4, 5 components</h1>
 			{check}
 			<Component4 />
 		</>
@@ -39,10 +40,18 @@ function Component3() {
 }
 
 function Component4() {
+  let { currentValue} = useContext(UserContext);
 	console.log("component 4 rendered");
+  currentValue = currentValue + 100;
+  console.log(currentValue, 'in component 4');
+  /* it doesnot cause re render since change in value is in local copy */
+  const doesNotCauseReRender = () => {
+    currentValue = currentValue + 200;
+    console.log(currentValue, 'in component 4 click');
+  }
 	return (
 		<>
-			<h1>Component 4</h1>
+			<h1 onClick={doesNotCauseReRender}>Click Component 4</h1>
 			<Component5 />
 		</>
 	);
@@ -51,9 +60,13 @@ function Component4() {
 const Component5 = () => {
 	const { user, setUser } = useContext(UserContext);
 	console.log("component 5 rendered");
+  const causeReRender = () => {
+    setUser("Changed User");
+    console.log("component 5 re-rendered");
+  }
 	return (
 		<>
-			<h1 onClick={() => setUser("lode")}>Component 5</h1>
+			<h1 onClick={causeReRender}>Click Component 5 to render all components</h1>
 			<h2>{`Hello ${user} again!`}</h2>
 		</>
 	);
